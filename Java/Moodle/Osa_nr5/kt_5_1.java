e888d243057305f3b5890c66f9e884511f03df73
Mashias <Matsiimon1829@gmail.com>
2021-02-25 00:11:55 +0200

    Andmebaas

--- Java/Moodle/Osa_nr5/kt_5_1.java
+++ Java/Moodle/Osa_nr5/kt_5_1.java
@@ -5,9 +5,29 @@ import java.util.Scanner;
 public class kt_5_1 {
     public static void main(String[] args) throws FileNotFoundException{
 
-        String[] andmed = new String[0];
+
         Scanner scan = new Scanner(new File("M:/Coding/Java/Moodle/Osa_nr5/andmebaas.txt"));
 
+        String[][] raamat = new String[0][0];
+        int lehekülgedeArv = 1;
+
+        while(scan.hasNext()){
+            String[][] ajutineRaamat = new String[lehekülgedeArv][raamat[lehekülgedeArv - 1].length + 1];
+
+            for(int i = 0; i < raamat.length; i++){
+                for(int j = 0; j < raamat[i].length; j++){
+                    ajutineRaamat[i][j] = raamat[i][j];
+                }  
+            }
+            ajutineRaamat[lehekülgedeArv - 1][ajutineRaamat[lehekülgedeArv - 1].length - 1] = scan.nextLine();
+            raamat = ajutineRaamat;
+
+            if(raamat[lehekülgedeArv - 1].length == 5){
+                lehekülgedeArv++;
+            }
+        }
+
+/*
         while(scan.hasNext()){
             String ajutineJarjend[] = new String[andmed.length + 1];
             for(int i = 0; i < andmed.length; i++){
@@ -16,6 +36,8 @@ public class kt_5_1 {
             ajutineJarjend[ajutineJarjend.length - 1] = scan.nextLine();
             andmed = ajutineJarjend;
         }
+
+*/
 
         System.out.println("See andmekogu koosneb mängu Counter Strike Global Offensive'i mängijate taustast. Hoiame mängija nime, SteamID'd, tiimi nime, mängusisest auastet, võidetud mängude arvu, mängule kulutatud aega ning viimase mängusessiooni kuupäeva.");
 
